30e8eda7b40f5ae0914b6549a5cb0ac322dcc199
Rucha <rucha.b@gmail.com>
2014-12-10 01:36:39 -0800

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ var express = require('express')
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+//app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.favicon());
