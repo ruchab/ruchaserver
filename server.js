453ba5cf2858e87b0e35329e0ef466c72b0f6a24
Rucha <rucha.b@gmail.com>
2014-12-11 16:24:32 -0800

    Update server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ var express = require('express')
 var app = express();
 var server = http.createServer(app);
 // all environments
-var server_port = process.env.PORT || 3000
-var server_ip_address = process.env.IP || '127.0.0.1'
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 app.set('port', server_port);
 //app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 //app.set('port', process.env.PORT || 8080);
